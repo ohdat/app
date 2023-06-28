6cb3651e334d6151b9e176c61e9024a6c8f70d29
Eric <eric@baochi.xin>
2023-06-28 19:01:30 +0800

    fix

--- utils/discord.go
+++ utils/discord.go
@@ -2,22 +2,19 @@ package utils
 
 import (
 	"strconv"
-	"sync"
-
-	"github.com/ohdat/app/utils/discord"
 )
 
-var sfnode *discord.Node
-var dcsfOnce sync.Once
+// var sfnode *discord.Node
+// var dcsfOnce sync.Once
 
-func GetDcSnowflake() *discord.Node {
-	dcsfOnce.Do(func() {
-		sfnode, _ = discord.NewNode()
-	})
-	return sfnode
-}
+// func GetDcSnowflake() *discord.Node {
+// 	dcsfOnce.Do(func() {
+// 		sfnode, _ = discord.NewNode()
+// 	})
+// 	return sfnode
+// }
 
 // DCNonce returns a snowflake string that can be used as a nonce for Discord.
 func DCNonce() string {
-	return strconv.Itoa(int(GetDcSnowflake().Generate()))
+	return strconv.Itoa(int(NextSnowflakeID()))
 }
